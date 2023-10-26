c4c30c2c88b10aca6abcb635413ba47d53680500
kunareddy harshareddy <kunareddyharshareddy@gmail.com>
2023-10-26 14:25:03 +0530

    commit7

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,17 @@
 // About.js
 import React from 'react';
 import './About.css';
+import one from "../photos/my.jpg";
 
 const About = () => {
   return (
     <section className="about-section">
+        <h2 align="center">About Me</h2>
 
       <div className="profile-img">
-        <img src="" alt="profile" />
+        <img src={one} className='img' alt="profile" />
       </div>
       <div className="about-content">
-        <h2>About Me</h2>
         <p>
           Welcome! I'm a passionate developer with a love for creating
           web applications. With a background in Mern stack, I bring quality work to
